3835ccf86670eea29a2e0b21d9c43969814d9763
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-09-10 21:08:34 +0530

    revert last push

--- backend/server.js
+++ backend/server.js
@@ -5,30 +5,14 @@ import cloudinary from "cloudinary";
 
 dotenv.config({ path: "./backend/config/config.env" });
 
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config({ path: __dirname + "/.env" });
-}
+connectDatabase();
 
-connectDatabase().then(() => {
-  app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port : ${process.env.PORT}`);
-  });
-  cloudinary.v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port : ${process.env.PORT}`);
 });
 
-// all your routes should go here
-app.use(
-  "/some-route",
-  require(path.join(__dirname, "api", "routes", "route.js"))
-);
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend", "build")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-  });
-}
+cloudinary.v2.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
