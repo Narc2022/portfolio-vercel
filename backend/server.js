adf5aa9dac80657e937b1871693a0ffd457fee15
Sachin Singh <102596511+Narc2022@users.noreply.github.com>
2023-09-10 21:03:04 +0530

    vercel last changes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,34 @@
-import{app} from "./app.js"
+import { app } from "./app.js";
 import dotenv from "dotenv";
-import {connectDatabase} from "./config/database.js";
+import { connectDatabase } from "./config/database.js";
 import cloudinary from "cloudinary";
 
-dotenv.config({path:"./backend/config/config.env"});
+dotenv.config({ path: "./backend/config/config.env" });
 
-connectDatabase();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: __dirname + "/.env" });
+}
 
-app.listen(process.env.PORT,() =>{
+connectDatabase().then(() => {
+  app.listen(process.env.PORT, () => {
     console.log(`Server is running on port : ${process.env.PORT}`);
-})
-
-cloudinary.v2.config({
-    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
-    api_secret:process.env.CLOUDINARY_API_SECRET,
+  });
+  cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 });
 
+// all your routes should go here
+app.use(
+  "/some-route",
+  require(path.join(__dirname, "api", "routes", "route.js"))
+);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
